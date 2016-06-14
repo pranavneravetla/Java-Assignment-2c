997990e9018ae00bb2e9f1ed6f851944836dfac4
pranavneravetla <pranavneravetla@gmail.com>
2016-06-14 13:02:49 +0530

    Add files via upload

---
+++ Assignment2c.java
@@ -0,0 +1,17 @@
+
+public class Assignment2c {
+
+	public static void main(String[] args) {
+		int num1=7.6;
+		int num2=bin;
+		
+		
+				
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
